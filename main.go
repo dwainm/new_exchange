d1eb1634f543ee2b82b5de934ad9132c25a14d09
Dwain Maralack <dwain.maralack@gmail.com>
2022-05-10 10:32:59 +0200

    Add basic place order mechanism to add an order to an in-memory order book with tests for the order ID generated

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var orderBookIDIncrementor = 0
 
 type Order struct {
 	product   string
 	orderType string
 	qty       int
 	amount    int
+	timestamp int64
+}
+
+type OrderBookEntry struct {
+	o            *Order
+	confirmation string
+}
+
+// @todo possible issues with pass by value
+func placeOrder(o *Order, ob []OrderBookEntry) OrderBookEntry {
+	orderBookIDIncrementor++
+	confirmation := strconv.Itoa(orderBookIDIncrementor) + "-" + o.orderType + "-" + o.product
+	newEntry := OrderBookEntry{o, confirmation}
+	ob = append(ob, newEntry)
+	return newEntry
 }
 
 func newOrder(p string, ot string, q int, a int) *Order {
-	o := Order{product: p, orderType: ot, qty: q, amount: a}
+	o := Order{product: p, orderType: ot, qty: q, amount: a, timestamp: time.Now().Unix()}
 	return &o
 }
 
--- main_test.go
+++ main_test.go
@@ -2,6 +2,16 @@ package main
 
 import "testing"
 
+func TestPlaceOrderConfirmationIdGeneration(t *testing.T) {
+	ob := make([]OrderBookEntry, 100, 100)
+	o := newOrder("ZZZ", "sell", 5, 200)
+	got := placeOrder(o, ob).confirmation
+	want := "1-sell-ZZZ"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestNewOrder(t *testing.T) {
 	got := newOrder("ZZZ", "bid", 5, 200)
 	want := "ZZZ"
