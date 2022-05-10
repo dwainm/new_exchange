f1ddff55b40e52f12fd13959e26aa503450c1f3a
Dwain Maralack <dwain.maralack@gmail.com>
2022-05-10 12:00:30 +0200

    Fix failing TestPlaceOrderInvalidType test

--- main_test.go
+++ main_test.go
@@ -16,7 +16,7 @@ func TestPlaceOrderInvalidType(t *testing.T) {
 	ob := make([]OrderBookEntry, 100, 100)
 	o := newOrder("ZZZ", SELL, 5, 200)
 	got := placeOrder(o, ob).confirmation
-	want := "1-sell-ZZZ"
+	want := "1-SELL-ZZZ"
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
