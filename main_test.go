5115f7c377aae144edce6836f26c9e5010f7e674
Dwain Maralack <dwain.maralack@gmail.com>
2022-05-10 10:57:58 +0200

    Update usages to constants. This is still not perfect as strings can still be passed to the function over the constant

--- main_test.go
+++ main_test.go
@@ -12,8 +12,18 @@ func TestPlaceOrderConfirmationIdGeneration(t *testing.T) {
 	}
 }
 
+func TestPlaceOrderInvalidType(t *testing.T) {
+	ob := make([]OrderBookEntry, 100, 100)
+	o := newOrder("ZZZ", SELL, 5, 200)
+	got := placeOrder(o, ob).confirmation
+	want := "1-sell-ZZZ"
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestNewOrder(t *testing.T) {
-	got := newOrder("ZZZ", "bid", 5, 200)
+	got := newOrder("ZZZ", BUY, 5, 200)
 	want := "ZZZ"
 	if got.product != want {
 		t.Errorf("got %q want %q", got.product, want)
