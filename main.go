3c643a32588387cfacf97da6fe6fefec0c9f93af
Dwain Maralack <dwain.maralack@gmail.com>
2022-05-10 10:49:54 +0200

    Add a custom enum type for BUY and SELL to avoid using open ended strings

--- main.go
+++ main.go
@@ -8,9 +8,26 @@ import (
 
 var orderBookIDIncrementor = 0
 
+type OrderType string
+
+const (
+	BUY  OrderType = "BUY"
+	SELL           = "SELL"
+)
+
+func (t OrderType) String() string {
+	if t == BUY {
+		return "BUY"
+	} else if t == SELL {
+		return "SELL"
+	} else {
+		return "unknown"
+	}
+}
+
 type Order struct {
 	product   string
-	orderType string
+	orderType OrderType
 	qty       int
 	amount    int
 	timestamp int64
@@ -24,13 +41,13 @@ type OrderBookEntry struct {
 // @todo possible issues with pass by value
 func placeOrder(o *Order, ob []OrderBookEntry) OrderBookEntry {
 	orderBookIDIncrementor++
-	confirmation := strconv.Itoa(orderBookIDIncrementor) + "-" + o.orderType + "-" + o.product
+	confirmation := strconv.Itoa(orderBookIDIncrementor) + "-" + o.orderType.String() + "-" + o.product
 	newEntry := OrderBookEntry{o, confirmation}
 	ob = append(ob, newEntry)
 	return newEntry
 }
 
-func newOrder(p string, ot string, q int, a int) *Order {
+func newOrder(p string, ot OrderType, q int, a int) *Order {
 	o := Order{product: p, orderType: ot, qty: q, amount: a, timestamp: time.Now().Unix()}
 	return &o
 }
--- main_test.go
+++ main_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestPlaceOrderConfirmationIdGeneration(t *testing.T) {
 	ob := make([]OrderBookEntry, 100, 100)
-	o := newOrder("ZZZ", "sell", 5, 200)
+	o := newOrder("ZZZ", SELL, 5, 200)
 	got := placeOrder(o, ob).confirmation
-	want := "1-sell-ZZZ"
+	want := "1-SELL-ZZZ"
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
